84d52345f5c4974481f343a30df8331d1082b577
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2024-09-19 18:49:49 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,22 +2,37 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().strip().split())
-
-# 각 국가의 메달 정보를 입력받음
-wax = [[int(x) for x in input().strip().split()] for _ in range(N)]
-
-# 금, 은, 동메달 수를 기준으로 내림차순 정렬
-wax.sort(key=lambda x: (-x[1], -x[2], -x[3]))
-
-# 등수 계산
+me = []
+for _ in range(N):
+    n,g,e,d = map(int, input().strip().split())
+    me.append((n,g,e,d))
+me.sort(key = lambda x : (x[1],x[2],x[3]),reverse=True)
 rank = 1
-idx2 = 0
-for i in range(N):
-    if wax[i][0] == K:
-        print(rank)
-        break
-    if i > 0 and wax[i][1] == wax[i-1][1] and wax[i][2] == wax[i-1][2] and wax[i][3] == wax[i-1][3]:
-        idx2 += 1
+ranks = 0
+tmp = [me[0][1],me[0][2],me[0][3]]
+
+for i in range(1,N):
+    n,g,e,d = me[i]
+    if tmp[0] == g and tmp[1] == e and tmp[2] == d:
+        ranks += 1
     else:
-        rank += 1 + idx2
-        idx2 = 0
+        rank += ranks + 1
+        ranks = 0
+    tmp[0] = g
+    tmp[1] = e
+    tmp[2] = d
+    if n == K:
+        print(rank)
+        sys.exit()
+
+print(1)
+
+
+
+
+
+
+
+
+
+
