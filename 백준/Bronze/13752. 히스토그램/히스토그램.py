12a9ebabd481207086a88043b26721c39f48bf58
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-01-01 03:07:33 +0900

    [Bronze IV] Title: 히스토그램, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+for _ in range(n):
+    x = int(input().strip())
+    for i in range(x):
+        print("=",end = "")
+    print()
+
+
+
+
+
+
+
+
+
