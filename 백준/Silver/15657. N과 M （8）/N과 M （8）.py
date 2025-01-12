d1f21a30046912fff8cdb1ceed3029fdcdefd053
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-01-13 00:23:57 +0900

    [Silver III] Title: N과 M (8), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def recur(lis):
+    if len(lis) == M:
+        if M == 1:
+            print(lis[0])
+        else:
+            print(' '.join(map(str,lis)))
+        return
+    
+    for i in range(len(li)):
+        if lis and lis[-1] > li[i]:
+            continue 
+        lis.append(li[i])
+        recur(lis)
+        lis.pop()
+
+
+N,M = map(int, input().strip().split())
+li = list(map(int, input().strip().split()))
+li.sort()
+recur([])
