e8a936fc34524d43e799e4bfedb16b7932a3e6eb
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-04-14 13:07:23 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 304 ms, Memory: 18276 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		for (int i = N; i > 0; i--) {
+			for (int j = N; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
