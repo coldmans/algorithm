bdfab8c32276d60d356022c8ab0f4c18d5eb0d48
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-04-25 17:41:23 +0900

    Title: 정수 입력받아 배수 출력, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250425/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int result = N;
+        for (int i = 0; i < 5; i++){
+            System.out.print(result + " ");
+            result += N;
+        }
+    }
+}
