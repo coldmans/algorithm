11e8bb7f94c91c68bd9e5be15e9a3dd1f0b5866c
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-04-25 17:43:45 +0900

    Title: B부터 A까지 감소, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250425/B부터 A까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        for(int i = B; i >= A; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
