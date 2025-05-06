a040fbfd40c73513740b38168c41b8daec5b0e0b
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-06 15:34:42 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 126ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250506/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    static void check(int[] x, int[] y){
+        int ready = -1;
+        for(int i = 0; i < x.length; i++){
+            if(x[i] == y[0]){
+                ready = i;
+                break;
+            }
+        }
+        if(ready == -1){
+            System.out.println("No");
+        }
+        int j = 0;
+        int i = ready;
+        int ch = 1;
+        while(i < x.length && j < y.length){
+            if(x[i] != y[j]){
+                System.out.println("No");
+                ch = 0;
+                break;
+            }
+            i += 1;
+            j += 1;
+        }
+        if(ch == 1){
+            System.out.println("Yes");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int[] a = new int[n1];
+        int[] b = new int[n2];
+        for (int i = 0; i < n1; i++)
+            a[i] = sc.nextInt();
+        for (int i = 0; i < n2; i++)
+            b[i] = sc.nextInt();
+        // Please write your code here.
+        check(a,b);
+    }
+}
