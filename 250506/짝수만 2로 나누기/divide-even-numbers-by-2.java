ae31b58df4d619784ae280bbe8703a7d8b0ceab0
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-06 16:57:20 +0900

    Title: 짝수만 2로 나누기, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 250506/짝수만 2로 나누기/divide-even-numbers-by-2.java
+++ 250506/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -14,7 +14,7 @@ public class Main {
         for(int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
         // Please write your code here.
-        zzar(arr);
+        zzac(arr);
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
