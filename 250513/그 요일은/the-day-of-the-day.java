cdeb3cde494829bc34c1877f70b3c1d9d34f8645
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-13 17:08:20 +0900

    Title: 그 요일은, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250513/그 요일은/the-day-of-the-day.java
+++ 250513/그 요일은/the-day-of-the-day.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m1 = sc.nextInt();
-        int d1 = sc.nextInt();
-        int m2 = sc.nextInt();
-        int d2 = sc.nextInt();
-        String A = sc.next();
-        // Please write your code here.
-        int n = m1 * 31 + d1 - (m2 * 31 + d2);
-        n *= -1;
-        System.out.println(n/7);
+import java.util.*;
+public class Main{
+    public static void main(String[]a){
+        int[]md={0,31,29,31,30,31,30,31,31,30,31,30,31};
+        Scanner s=new Scanner(System.in);
+        int m1=s.nextInt(),d1=s.nextInt(),m2=s.nextInt(),d2=s.nextInt();
+        String[]w={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+        String A=s.next();
+        int st=0,ed=0;
+        for(int i=1;i<m1;i++)st+=md[i];
+        for(int i=1;i<m2;i++)ed+=md[i];
+        st+=d1; ed+=d2;
+        int idx=0;
+        while(!w[idx].equals(A))idx++;
+        int cnt=0;
+        for(int i=st;i<=ed;i++)if((i-1)%7==idx)cnt++;
+        System.out.print(cnt);
     }
 }
