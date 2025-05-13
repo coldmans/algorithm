4f3b443904232bc7aeefc64512517a33cfcf6caa
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-13 17:09:09 +0900

    Title: 그 요일은, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250513/그 요일은/the-day-of-the-day.java
+++ 250513/그 요일은/the-day-of-the-day.java
@@ -1,19 +1,20 @@
 import java.util.*;
 public class Main{
     public static void main(String[]a){
-        int[]md={0,31,29,31,30,31,30,31,31,30,31,30,31};
         Scanner s=new Scanner(System.in);
+        int[]md={0,31,29,31,30,31,30,31,31,30,31,30,31};
         int m1=s.nextInt(),d1=s.nextInt(),m2=s.nextInt(),d2=s.nextInt();
-        String[]w={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+        String[]days={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
         String A=s.next();
-        int st=0,ed=0;
-        for(int i=1;i<m1;i++)st+=md[i];
-        for(int i=1;i<m2;i++)ed+=md[i];
-        st+=d1; ed+=d2;
+        int start=0,end=0;
+        for(int i=1;i<m1;i++)start+=md[i];
+        for(int i=1;i<m2;i++)end+=md[i];
+        start+=d1; end+=d2;
+        int diff=end-start+1;
         int idx=0;
-        while(!w[idx].equals(A))idx++;
-        int cnt=0;
-        for(int i=st;i<=ed;i++)if((i-1)%7==idx)cnt++;
+        for(;!days[idx].equals(A);idx++);
+        int first=(7-((start-1+idx)%7))%7+1;
+        int cnt=(diff-first)/7+((diff>=first)?1:0);
         System.out.print(cnt);
     }
 }
