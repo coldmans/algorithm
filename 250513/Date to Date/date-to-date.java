a311785a089c8254489ec3d801f1138fe866b6a9
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-13 16:09:33 +0900

    Title: Date to Date, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250513/Date to Date/date-to-date.java
+++ 250513/Date to Date/date-to-date.java
@@ -8,6 +8,10 @@ public class Main {
         int d2 = sc.nextInt();
         // Please write your code here.
         int days = 0;
+        if(m1 == m2){
+            System.out.println(d2-d1+1);
+            System.exit(0);
+        }
         if(m1 == 1 || m1 == 3 || m1 == 5 || m1 == 7 || m1 == 8 || m1 == 10 || m1 == 12){
             days = 31 - d1;
         }
@@ -15,7 +19,7 @@ public class Main {
             days = 30 - d1;
         }
         m1 += 1;
-        while(true){
+        while(m1 <= m2){
             if(m1 == 1 || m1 == 3 || m1 == 5 || m1 == 7 || m1 == 8 || m1 == 10 || m1 == 12){
                 if(m1 == m2){
                     days += d2;
