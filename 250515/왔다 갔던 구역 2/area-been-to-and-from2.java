351efb2616912e424052df53d7dd06c9f18a682f
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-15 12:55:21 +0900

    Title: 왔다 갔던 구역 2, Time: 146ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250515/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250515/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         Map<Integer, Integer> visit = new HashMap<>();
         int point = 0;
+        int total = 0;
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
@@ -14,26 +16,20 @@ public class Main {
             if(dir == 'L'){
                 for(int j = point-1; j >= point-x; j--){
                     visit.put(j, visit.getOrDefault(j,0) + 1);
+                    if(visit.get(j) == 2) total++;
                 }
                 point -= x;
             }
             else if(dir == 'R'){
                 for(int j = point+1; j <= point+x; j++){
                     visit.put(j, visit.getOrDefault(j,0) + 1);
+                    if(visit.get(j) == 2) total++;
                 }
                 point += x;
             }
             
             
         }
-
-
-        int result = 0;
-        for(int v : visit.values()){
-            if(v >= 2){
-                result++;
-            }
-        }
-        System.out.println(result);
+        System.out.println(total);
     }
 }
