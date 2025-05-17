8725a1eecf8ac2c2c5bdd5c95c6b240afb611ddf
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-18 01:15:50 +0900

    Title: 입력받아 연산하기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250518/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + 87);
+        System.out.println(b % 10);
+    }
+}
