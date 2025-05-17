751f75f2f06ad612b589cb544a732345316d73c5
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-17 17:52:09 +0900

    Title: 간단한 사칙연산, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250517/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+
+    }
+}
