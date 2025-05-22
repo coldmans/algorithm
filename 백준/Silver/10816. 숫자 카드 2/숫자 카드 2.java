21d6b56d9966b5dfd2e5090097051b4964a52474
jinhyeong jang <159500222+coldmans@users.noreply.github.com>
2025-05-22 14:51:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 984 ms, Memory: 173124 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,34 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        // 입력을 빠르게 받기 위한 BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String args[]) {
-		Scanner scanner = new Scanner(System.in);
-		HashMap<Integer, Integer> hash = new HashMap<>();
-		int N = scanner.nextInt();
-		for(int i = 0; i < N; i++) {
-			int x = scanner.nextInt();
-			if(hash.containsKey(x)) {
-				int t = hash.get(x);
-				hash.put(x, t+1);
-			}
-			else {
-				hash.put(x, 1);
-			}
-		}
-        int M = scanner.nextInt();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            int target = scanner.nextInt();
-            sb.append(hash.getOrDefault(target, 0)).append(" ");
+        // 카드 개수
+        int N = Integer.parseInt(br.readLine());
+
+        // 숫자 카드 입력
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        System.out.println(sb.toString());
-		
-		
-	}
-	
+        // 질문 개수
+        int M = Integer.parseInt(br.readLine());
+
+        // 질문 숫자 입력 + 출력 준비
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(target, 0)).append(' ');
+        }
+
+        // 결과 출력
+        System.out.println(sb);
+    }
 }
-
-
