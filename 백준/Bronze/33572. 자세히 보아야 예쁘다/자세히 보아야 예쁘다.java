8b608524ff0cb1bcb0f48f7bc87966e8380659da
jinhyeong jang <jinhinjala26@naver.com>
2025-10-03 17:33:27 +0900

    [Bronze II] Title: 자세히 보아야 예쁘다, Time: 408 ms, Memory: 59496 KB -BaekjoonHub

---
+++ 백준/Bronze/33572. 자세히 보아야 예쁘다/자세히 보아야 예쁘다.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        BigInteger M = new BigInteger(st.nextToken());
+        
+        BigInteger totalTime = BigInteger.ZERO;
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            // Integer.parseInt 대신 BigInteger 직접 생성!
+            BigInteger A = new BigInteger(st.nextToken());
+            totalTime = totalTime.add(A.subtract(BigInteger.ONE));
+        }
+        
+        if(totalTime.compareTo(M) >= 0){
+            System.out.println("DIMI");
+        }
+        else {
+            System.out.println("OUT");
+        }
+    }
+}
