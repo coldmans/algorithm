08956fb297b97e8b58e2336cf8b84d7feb08ace5
jinhyeong jang <jinhinjala26@naver.com>
2026-02-11 21:15:39 +0900

    [level 3] Title: 단속카메라, Time: 8.82 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (a , b) -> Integer.compare(a[1], b[1]));
+        
+        int count = 0;
+        int lastCamera = -30001;
+        
+        for(int[] route : routes){
+            if(route[0] > lastCamera){
+                count++;
+                lastCamera = route[1];
+            }
+        }
+        
+        return count;
+    }
+}
