9203d1f038c2a4cd2a1ce8f5ac48e4650e606b3f
jinhyeong jang <jinhinjala26@naver.com>
2026-02-17 14:34:02 +0900

    [level 1] Title: 예산, Time: 1.12 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        
+        Arrays.sort(d);
+        int ctt = 0;
+        for(int i = 0; i < d.length; i++){
+            if(budget < d[i]){
+                break;
+            }
+            budget -= d[i];
+            ctt ++;
+        }
+        return ctt;
+    }
+}
