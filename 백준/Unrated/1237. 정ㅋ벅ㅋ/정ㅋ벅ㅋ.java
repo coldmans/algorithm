0455b63b465df056953af4b0104782575987de0e
jinhyeong jang <jinhinjala26@naver.com>
2026-02-21 21:48:29 +0900

    [Unrated] Title: 정ㅋ벅ㅋ, Time: 96 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Unrated/1237. 정ㅋ벅ㅋ/정ㅋ벅ㅋ.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        
+
+        System.out.println("문제의 정답");
+
+    }
+}
