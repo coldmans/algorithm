f023e8be8a962e5cce1c7d00d729341367d0d95f
jinhyeong jang <jinhinjala26@naver.com>
2026-08-04 22:55:01 +0900

    [level 3] Title: 부대복귀, Time: 80.28 ms, Memory: 209 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+        for(int []r : roads){
+            graph.get(r[0]).add(r[1]);
+            graph.get(r[1]).add(r[0]);
+        }
+        
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, -1);
+        dist[destination] = 0;
+        
+        Deque<Integer> q = new ArrayDeque<>();
+        q.add(destination);
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            for(int next : graph.get(cur)){
+                if(dist[next] == -1){
+                    dist[next] = dist[cur] + 1;
+                    q.add(next);
+                }
+            }
+        }
+        
+        int[] answer = new int[sources.length];
+        for(int i = 0; i < sources.length; i++) answer[i] = dist[sources[i]];
+        return answer;
+    }
+}
