cc2dd18980aaf3a72157f241845784cd59348bbd
jinhyeong jang <jinhinjala26@naver.com>
2026-08-04 22:04:43 +0900

    [level 1] Title: 기사단원의 무기, Time: 4.56 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int []num = new int[number+1];
+        for(int i = 1; i <= number; i++){
+            for(int j = i; j <= number; j+=i){
+                num[j] += 1;
+            }
+        }
+        for(int i = 1; i <= number; i++){
+            if(num[i] > limit){
+                answer += power;
+            }
+            else{
+                answer += num[i];
+            }
+        }
+        return answer;
+    }
+}
