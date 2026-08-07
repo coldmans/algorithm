0c9dab187871c5cfa9af127f902a6d16bac9d75f
jinhyeong jang <jinhinjala26@naver.com>
2026-08-07 10:43:50 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.88 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,54 +2,68 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        int answer = 0;
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            if(a[0] == b[0]){
-                if(a[1] == b[1]){
-                    return a[2] - b[2];
-                }
+        PriorityQueue<int []> pq = new PriorityQueue<>((a,b) -> {
+            if(a[2] != b[2]){
+                return a[2] - b[2];
+            }
+            if(a[1] != b[1]){
                 return a[1] - b[1];
+            }
+            if(a[0] != b[0]){
+                return a[0] - b[0];
             }
             return a[0] - b[0];
         });
         
-        
-        PriorityQueue<int[]> tmpQ = new PriorityQueue<>((a,b) -> {
-            return a[0] - b[0];
+        PriorityQueue<int []> tq = new PriorityQueue<>((a,b) -> {
+            if(a[1] != b[1]){
+                return a[1] - b[1];
+            }
+            return a[1] - b[1];
         });
         
         for(int i = 0; i < jobs.length; i++){
-            tmpQ.add(new int[]{jobs[i][0], jobs[i][1], i});
+            tq.add(new int[]{i, jobs[i][0], jobs[i][1]});
         }
         
-        int zak = 0;
-        int tt = 0;
-        for(int i = 0; i <= 10000001; i++){
-            while(!tmpQ.isEmpty() && tmpQ.peek()[0] == i){
-                int []tmp = tmpQ.poll();
-                pq.add(new int[]{tmp[1], tmp[0], tmp[2]});
+        int i = 0;
+        int time = 0;
+        int wow = 0;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        while(true){
+            if(tq.isEmpty() && pq.isEmpty() && time == 0){
+                break;
             }
             
-            if(zak > 0){
-                zak -= 1;
-                if(zak == 0){
-                    answer += i - tt;
-                    System.out.println(answer);
-                    tt = 0;
+            while(!tq.isEmpty() && tq.peek()[1] <= i){
+                pq.add(tq.poll());
+            }
+            
+            if(pq.isEmpty() && !tq.isEmpty()){
+                int []tmp = tq.poll();
+                pq.add(tmp);
+                i = tmp[1];
+                while(!tq.isEmpty() && tq.peek()[1] == i){
+                    pq.add(tq.poll());
                 }
             }
             
-            if(zak == 0 && tmpQ.isEmpty() && pq.isEmpty()){
-                break;
-            }
-            if(zak == 0 && !pq.isEmpty()){
+            if(!pq.isEmpty()){
                 int []tmp = pq.poll();
-                zak += tmp[0];
-                tt = tmp[1];
+                i += tmp[2];
+                list.add(i - tmp[1]);
             }
+            
+        
         }
         
-        return answer/jobs.length;
+        int py = 0;
+        for(int j = 0; j < list.size(); j++){
+            py += list.get(j);
+            
+        }
+        return py / list.size();
     }
 }
