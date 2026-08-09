b6e1f7625c120ccfdd74187e552b66f6f3187da2
jinhyeong jang <jinhinjala26@naver.com>
2026-08-09 14:46:45 +0900

    [level 2] Title: [1차] 캐시, Time: 26.24 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        if (cacheSize == 0) return cities.length * 5;
+
+        int answer = 0;
+        LinkedList<String> cache = new LinkedList<>();  
+
+        for (String c : cities) {
+            String city = c.toLowerCase();
+            if (cache.remove(city)) {         
+                answer += 1;
+            } else {                            
+                answer += 5;
+                if (cache.size() == cacheSize) cache.removeFirst();
+            }
+            cache.addLast(city);               
+        }
+        return answer;
+    }
+}
